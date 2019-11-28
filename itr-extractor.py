5a8389d6d99af0b56de92d8d9dbe5f6d9321d73e
MClarkTurner <mclarktu@fandm.edu>
2019-11-28 17:04:19 -0500

    add itr-extract

--- itr-extractor.py
+++ itr-extractor.py
@@ -155,7 +155,7 @@ class ITR_Extractor:
 		for label in range(self.num_classes):
 			for itr in itr_set:
 				if(itr in self.tcgs[label]):
-					sum_values[i] += self.tcgs[label][itr]
+					sum_values[label] += self.tcgs[label][itr]
 
 			print("obtained: ",label,  sum_val)
 
