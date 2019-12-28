2e53d849775f9de07a3aa23305b614254fd1e2e4
MClarkTurner <mclarktu@fandm.edu>
2019-12-28 13:22:11 -0500

    sklean_npy

--- itr-sklearn_npy.py
+++ itr-sklearn_npy.py
@@ -118,7 +118,7 @@ class ITR_Extractor:
 		#self.clf = svm.SVC().fit(train_mat, np.array(self.labels))
 		#self.clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42,max_iter=5, tol=None).fit(train_mat, np.array(self.labels))
 		train_mat = np.array(self.corpus)
-		self.clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-4, random_state=42,max_iter=500, tol=None, verbose=0).fit(train_mat, np.array(self.labels))
+		self.clf = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-4, random_state=42,max_iter=250, tol=None, verbose=0).fit(train_mat, np.array(self.labels))
 
 	def pred(self, txt_file):
 		#https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
