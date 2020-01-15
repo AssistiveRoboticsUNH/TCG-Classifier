4607d89e95bab3f921fae21598c40a7d91647d13
MClarkTurner <mclarktu@fandm.edu>
2020-01-15 15:27:14 -0500

    interpretability

--- interp_test.py
+++ interp_test.py
@@ -52,7 +52,7 @@ te = TextExplainer(random_state=42)
 te.fit(corpus[0], pipe.predict_proba)
 out = te.show_prediction(target_names=[0,1])
 
-print(out.__repr__)
+print(out.__repr__())
 
 '''
 importance = clf.coef_.toarray()[0]
