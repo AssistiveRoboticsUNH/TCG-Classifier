61cf964cf0c499d6cd5fcac6567f42606459eb77
MClarkTurner <mclarktu@fandm.edu>
2020-01-16 13:02:08 -0500

    interpretability

--- interp_test.py
+++ interp_test.py
@@ -147,7 +147,8 @@ print(a.shape, b.shape)
 
 print(np.dot(a, b).shape)
 print(np.dot(a, b))
-print(np.argmax(np.dot(a, b)))
+print(np.argmax(np.dot(a, b), axis = 1))
+print(clf.decision_function)
 
 
 
