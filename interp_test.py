8158eb9a2f668bca6b71018b07442fdfb0bcd5bb
MClarkTurner <mclarktu@fandm.edu>
2020-01-15 22:39:36 -0500

    interpretability

--- interp_test.py
+++ interp_test.py
@@ -40,11 +40,13 @@ Yeval = [0,0,0,0,1,1,1,1]
 
 
 # define model
-tfidf = CountVectorizer()
-#tfidf = TfidfVectorizer(ngram_range=(1,1), sublinear_tf=True)
+#tfidf = CountVectorizer()
+tfidf = TfidfVectorizer(ngram_range=(1,1), sublinear_tf=True)
 clf = svm.SVC(max_iter=100, tol=1e-4, probability=True, 
 	kernel='linear', decision_function_shape='ovr' )
 pipe = make_pipeline(tfidf, clf)
+
+tfidf.fit_transform(Xtrain)
 
 # fit model
 pipe.fit(Xtrain, Ytrain)
