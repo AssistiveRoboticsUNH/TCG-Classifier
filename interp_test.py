3d5c86e2849d93351967b75e84adc28725cb68b4
MClarkTurner <mclarktu@fandm.edu>
2020-01-16 15:18:56 -0500

    interpretability

--- interp_test.py
+++ interp_test.py
@@ -174,7 +174,7 @@ for i in range(n_class):
 		votes[pred[:, k] == 1, j] += 1
 		k+=1
 
-trans_conf = sum_of_conf / (3 * np.abs(sum_of_conf) +1)
+trans_conf = sum_of_conf / (3 * (np.abs(sum_of_conf) +1))
 out = votes + trans_conf
 
 print(out)
