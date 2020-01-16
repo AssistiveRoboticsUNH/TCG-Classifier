de2998ab5df038c4155305a63db5f12e06f0664c
MClarkTurner <mclarktu@fandm.edu>
2020-01-16 11:17:23 -0500

    interpretability

--- interp_test.py
+++ interp_test.py
@@ -98,7 +98,7 @@ Yeval = [0,1,2,0,1,2]
 tfidf = CountVectorizer()
 #tfidf = TfidfVectorizer(ngram_range=(1,1), sublinear_tf=True)
 clf = svm.SVC(max_iter=100, tol=1e-4, probability=True, 
-	kernel='linear', decision_function_shape='ovo' )
+	kernel='linear', decision_function_shape='ovr' )
 
 #clf = svm.LinearSVC( )
 
@@ -129,6 +129,8 @@ for label in range(coef.shape[0]):
 
 	for f, c in zip(fn, cf):
 		print(f, c)
+
+print(clf.intercept_)
 
 
 '''
