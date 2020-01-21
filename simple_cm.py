5bfc11173b47e070ae99e9213348f0d984154c2b
MClarkTurner <mclarktu@fandm.edu>
2020-01-20 23:05:31 -0500

    interpretability

--- simple_cm.py
+++ simple_cm.py
@@ -101,7 +101,7 @@ import matplotlib.pyplot as plt
 
 from sklearn import svm, datasets
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import plot_confusion_matrix
+#from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import confusion_matrix
 
 
