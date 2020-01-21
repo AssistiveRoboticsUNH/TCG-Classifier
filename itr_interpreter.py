cb36efd9bf3b8a51f5bff825b4b289ea6e0c49ce
MClarkTurner <mclarktu@fandm.edu>
2020-01-20 19:43:40 -0500

    interpretability

--- itr_interpreter.py
+++ itr_interpreter.py
@@ -231,7 +231,7 @@ def generate_top_bottom_table(tcg, label, count=10, out="feature_importance.png"
 		print("names")
 		print(names)
 
-		plt.yticks(range(count*2), names)
+		#plt.yticks(range(count*2), names)
 		plt.gca().invert_yaxis()
 		plt.title("BEFORE, Depth 0")
 	else:
