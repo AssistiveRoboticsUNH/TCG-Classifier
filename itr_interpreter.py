12b42ffabcb23ef3e1bb6fea16118b65f8142729
MClarkTurner <mclarktu@fandm.edu>
2020-01-27 16:39:19 -0500

    interp

--- itr_interpreter.py
+++ itr_interpreter.py
@@ -485,7 +485,20 @@ def visualize_example(ex, sess, input_placeholder, activation_map, feature_dict,
 	for ams in important_am:
 		print(ams.shape)
 
+	num_frames = len(important_am[0][1])
 
+	#max_window_scale = [2, 2, 2, 4, 8]
+	radius = 10
+
+	print("raw_data.shape:", raw_data.shape)
+	src = raw_data[0]
+	x, y = 50,50
+	ovl = cv2.circle(src, (x-radius, y-radius), radius, (255,0,0), -1)
+
+	alpha=0.5
+	cv2.addWeighted(src, alpha, ovl, 1 - alpha, 0, ovl)
+
+	cv2.imwrite("viz_spat.png", ovl)
 	
 		
 
