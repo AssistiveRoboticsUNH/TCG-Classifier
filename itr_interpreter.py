3761907a5b98cc996f5e09b18808d8727ff4521a
MClarkTurner <mclarktu@fandm.edu>
2020-01-28 23:28:28 -0500

    interp

--- itr_interpreter.py
+++ itr_interpreter.py
@@ -471,7 +471,7 @@ def visualize_example(ex, sess, input_placeholder, activation_map, feature_dict,
 		src = cv2.cvtColor(src, cv2.COLOR_RGB2BGR)
 
 		b_channel, g_channel, r_channel = cv2.split(src)
-		src = Image.fromarray(cv2.merge((b_channel, g_channel, r_channel, np.ones_like(b_channel)*255 )))
+		src = Image.fromarray(cv2.merge((r_channel, g_channel, b_channel, np.ones_like(b_channel)*255 )))
 
 		stack = []
 
