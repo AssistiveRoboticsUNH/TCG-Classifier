f734567592587e11c3285b53b58233ab94952d29
MClarkTurner <mclarktu@fandm.edu>
2020-01-28 22:34:19 -0500

    interp

--- itr_interpreter.py
+++ itr_interpreter.py
@@ -527,14 +527,8 @@ def visualize_example(ex, sess, input_placeholder, activation_map, feature_dict,
 
 			for i in range(s.shape[0]):
 				for j in range(s.shape[1]):
-					opac = np.sum(s[i, j])/(255*3)
-					print("opac:", opac)
-
-					print("src[i, j]:", src[i, j], src[i, j] * (1-opac))
-					print("s[i, j]:", s[i, j], s[i, j] * (opac))
-					print("fin:", src[i, j])
-
-					src[i, j] = src[i, j] * (1-opac) + s[i, j] * opac
+					
+					src[i, j] = np.max(src[i, j]*0.5  s[i, j] * 0.5, 255)
 		
 	print(src[0, 0])
 	cv2.imwrite("viz_spat.png", src)
