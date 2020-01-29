50d612e0184e1bf3a89d39676df0374d13800007
MClarkTurner <mclarktu@fandm.edu>
2020-01-28 23:48:12 -0500

    interp

--- itr_interpreter.py
+++ itr_interpreter.py
@@ -459,7 +459,7 @@ def visualize_example(ex, sess, input_placeholder, activation_map, feature_dict,
 			am[..., row] = np.zeros_like(am[row])
 		else:
 			am[..., row] = (am[..., row] - min_max_vals["min"][depth][row]) / (min_max_vals["max"][depth][row] - min_max_vals["min"][depth][row])
-	am[< 0] = 0  
+	am[am< 0] = 0  
 
 	am *= 255
 	scaled_am = am.astype(np.uint8)
