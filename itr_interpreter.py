408440dff077aee4a25ea3d1686f76ff294cfe32
MClarkTurner <mclarktu@fandm.edu>
2020-01-30 09:11:36 -0500

    interp

--- itr_interpreter.py
+++ itr_interpreter.py
@@ -675,7 +675,7 @@ def visualize_example(tcg, ex, sess, input_placeholder, activation_map, feature_
 			print("radius:", r)
 			print("color:", c)
 
-			src = cv2.circle(src, max_point, r, c, 3)
+			src = cv2.circle(src, tuple(max_point), r, c, 3)
 
 
 
