64ea881e6b7c08dc9c516105865c485b80a78664
MClarkTurner <mclarktu@fandm.edu>
2020-02-23 17:06:40 -0500

    scaler

--- itr_sklearn.py
+++ itr_sklearn.py
@@ -212,7 +212,7 @@ class ITR_Extractor:
 		print(train_mat.shape)
 
 		t_s = time.time()
-		train_mat = self.scaler.fit_transform(train_mat)
+		#train_mat = self.scaler.fit_transform(train_mat)
 		train_mat = self.svd.fit_transform(train_mat)
 		print("TruncatedSVD: ", time.time()-t_s)
 		print(train_mat.shape)
@@ -225,13 +225,13 @@ class ITR_Extractor:
 		#https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
 		txt = self.parse_txt_file(txt_file)
 		data = self.tfidf.transform([txt])
-		data = self.scaler.transform(data)
+		#data = self.scaler.transform(data)
 		data = self.svd.transform(data)
 		return self.clf.predict(data)
 
 	def eval(self):
 		data = self.tfidf.transform(self.evalcorpus)
-		data = self.scaler.transform(data)
+		#data = self.scaler.transform(data)
 		data = self.svd.transform(data)
 		pred = self.clf.predict(data)
 		return metrics.accuracy_score(self.evallabels, pred)
@@ -266,7 +266,7 @@ class ITR_Extractor:
 
 		self.tfidf = TfidfVectorizer(token_pattern=r"\b\w+-\w+-\w+\b", sublinear_tf=True)
 		self.scaler = StandardScaler(with_mean=False)
-		self.svd = TruncatedSVD(n_components=100)
+		self.svd = TruncatedSVD(n_components=1000)
 		self.clf = svm.SVC(max_iter=1000, tol=1e-4, probability=True, kernel='linear', decision_function_shape='ovr')
 		
 		if(save_name != ""):
