98008a20776eda67f4daab37ca1e2f602cd09a23
MClarkTurner <mclarktu@fandm.edu>
2020-02-24 02:39:41 -0500

    scaler

--- gcn.py
+++ gcn.py
@@ -68,8 +68,24 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		print("fitting model...")
 		print("len(tcg.corpus):", len(tcg.corpus))
 
+		batch_size = 4
+
 		data_in = np.array(tcg.tfidf.fit_transform(tcg.corpus).toarray())
 		data_label = np.array(tcg.labels)
+
+		trainloader = torch.utils.data.DataLoader(zip(data_in, data_label), batch_size=batch_size,
+										  shuffle=True, num_workers=2)
+
+
+		# CLASSIFY 
+		for ex in test_data:
+			tcg.add_file_to_eval_corpus(ex[path], ex['label'], ex['label_name'])
+
+		eval_in = np.array(tcg.tfidf.transform(tcg.evalcorpus).toarray())
+		eval_label = np.array(tcg.evallabels)
+
+		testloader = torch.utils.data.DataLoader(zip(eval_in, eval_label), batch_size=batch_size,
+										 shuffle=False, num_workers=2)
 
 
 		
@@ -77,51 +93,71 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		print("data_label.shape", data_label.shape)
 
 		#model
-		x_ph = tf.compat.v1.placeholder(np.float32, [None, data_in.shape[1]])
-		y_ph = tf.compat.v1.placeholder(np.int32, [None])
+		import torch.nn as nn
+		class Net(nn.Module):
+			def __init__(self, input_size, num_classes):
+				super(Net, self).__init__()
+				self.dense = nn.Linear(input_size, num_classes)				
 
-		dense = tf.compat.v1.layers.dense(x_ph, num_classes)
-		pred  = tf.argmax(dense, axis=1)
+			def forward(self, x):
+				return self.dense(x)
+
+		net = Net(data_in.shape[1], num_classes)
+
+		import torch.optim as optim
+
+		criterion = nn.CrossEntropyLoss()
+		optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+		t_s = time.time()
+		for epoch in range(2):  # loop over the dataset multiple times
+
+			running_loss = 0.0
+			for i, data in enumerate(trainloader, 0):
+				# get the inputs; data is a list of [inputs, labels]
+				inputs, labels = data
+
+				# zero the parameter gradients
+				optimizer.zero_grad()
+
+				# forward + backward + optimize
+				outputs = net(inputs)
+				loss = criterion(outputs, labels)
+				loss.backward()
+				optimizer.step()
+
+				# print statistics
+				running_loss += loss.item()
+				if i % 2000 == 1999:    # print every 2000 mini-batches
+					print('[%d, %5d] loss: %.3f' %
+						  (epoch + 1, i + 1, running_loss / 2000))
+					running_loss = 0.0
 
 
-		loss  = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y_ph, logits=dense)
-		opt   = tf.compat.v1.train.AdamOptimizer(learning_rate=0.001)
+		print("train elapsed:", time.time()-t_s)
+			
 
-		train_op = opt.minimize(loss)
+		print("evaluating model...")
+		t_s = time.time()
+		total = 0
+		with torch.no_grad():
+			for data in testloader:
+				images, labels = data
+				outputs = net(images)
+				_, predicted = torch.max(outputs.data, 1)
+				total += labels.size(0)
+				correct += (predicted == labels).sum().item()
+		print("test elapsed:", time.time()-t_s)
 
-		
-
-		with tf.Session() as sess:
-			sess.run(tf.initialize_all_variables())
-
-			t_s = time.time()
-			for i in range(1000):
-				if(i % 100 == 0):
-					print(i)
-				sess.run(train_op, feed_dict={x_ph: data_in, y_ph: data_label})
-			print("train elapsed:", time.time()-t_s)
-		
-		
-
-
-			# CLASSIFY 
-			for ex in test_data:
-				tcg.add_file_to_eval_corpus(ex[path], ex['label'], ex['label_name'])
-
-			eval_in = np.array(tcg.tfidf.transform(tcg.evalcorpus).toarray())
-			eval_label = np.array(tcg.evallabels)
-
-			print("evaluating model...")
-			pred_val = sess.run(pred, feed_dict={x_ph: eval_in})
-
-			cur_accuracy = metrics.accuracy_score(eval_label, pred_val)
+		print('Accuracy of the network on the 10000 test images: %d %%' % (
+			100 * correct / total))
 
 		# if model accuracy is good then replace the old model with new save data
 		#if(cur_accuracy > max_accuracy):
 		#	tcg.save_model(os.path.join(save_dir, "model"))
 		#	max_accuracy = cur_accuracy
 
-		print("Training layer: {:d}, iter: {:d}/{:d}, acc:{:0.4f}, max_acc: {:0.4f}".format(layer, iteration, repeat, cur_accuracy, max_accuracy))
+		#print("Training layer: {:d}, iter: {:d}/{:d}, acc:{:0.4f}, max_acc: {:0.4f}".format(layer, iteration, repeat, cur_accuracy, max_accuracy))
 		
 
 if __name__ == '__main__':
