ae25c47ee12fdc589c771375d1b5befbfa207566
MClarkTurner <mclarktu@fandm.edu>
2020-02-25 23:22:09 -0500

    thunder svm

--- itr_sklearn_repeat.py
+++ itr_sklearn_repeat.py
@@ -55,6 +55,7 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		train_label_filename = os.path.join(dataset_dir, 'b_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id), 'train_label_{0}_{1}.spz'.format(ex['example_id'], layer))
 		test_label_filename  = os.path.join(dataset_dir, 'b_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id), 'test_label{0}_{1}.spz'.format(ex['example_id'], layer))
 
+		print(train_filename)
 		parse_data = not os.path.exists(train_filename)
 
 		if(parse_data):
