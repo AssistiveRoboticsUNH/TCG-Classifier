c92a201456d0694b372d4b2621fd6b91d9cee07d
MClarkTurner <mclarktu@fandm.edu>
2020-02-27 11:50:50 -0500

    c++ parser

--- itr_process.py
+++ itr_process.py
@@ -40,6 +40,7 @@ def parse_files(file_list, num_procs=1, empty_locs=None):
 
 	#corpus = [extract_wrapper(file_list[0]), extract_wrapper(file_list[1])]
 	corpus = np.array(corpus)
+	corpus.reshape(corpus.shape[0], -1)corpus
 
 	if(empty_locs == None):
 		empty_locs = np.where(corpus.any(axis=1))
@@ -47,7 +48,7 @@ def parse_files(file_list, num_procs=1, empty_locs=None):
 	print(empty_locs)
 	corpus = corpus[empty_locs]
 
-	return  scipy.sparse.csr_matrix(corpus.reshape(corpus.shape[0], -1)), empty_locs
+	return  scipy.sparse.csr_matrix(corpus), empty_locs
 
 
 
