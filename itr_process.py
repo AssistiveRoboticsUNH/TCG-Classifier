5a3f20c7d094680a666e172c13f97f89e8d42d61
MClarkTurner <mclarktu@fandm.edu>
2020-02-26 23:08:59 -0500

    c++ parser

--- itr_process.py
+++ itr_process.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 from itr_sklearn import ITR_Extractor
 
 from itr_parser import extract_itr_seq
-from sklearn.feature_extraction.text import TfidfVectorizer, HashingVectorizer, CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer, HashingVectorizer, CountVectorizer
 from sklearn.preprocessing import StandardScaler
 
 from multiprocessing import Pool
