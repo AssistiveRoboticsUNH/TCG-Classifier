c8cb8895444eff725b0b14716e517699017f2bd2
MClarkTurner <mclarktu@fandm.edu>
2020-02-27 09:21:16 -0500

    c++ parser

--- itr_parser.cpp
+++ itr_parser.cpp
@@ -7,7 +7,7 @@
 
 
 using namespace std;
-using namespace boost::python;
+namespace p = boost::python;
 namespace np = boost::python::numpy;
 
 class Event{
@@ -130,7 +130,7 @@ np::ndarray extract_itr_seq_into_counts(string txt_file){
 
 	// get a list of all of the ITRs in the txt_file
 	//int array[num_features][num_features][7] itr_list = {};
-	tuple shape = make_tuple(num_features, num_features, 7);
+	p::tuple shape = p::make_tuple(num_features, num_features, 7);
 	np::dtype dt = np::dtype::get_builtin<int>();
 	np::ndarray itr_list = np::zeros(shape, dt);
     //std::copy(v.begin(), v.end(), reinterpret_cast<double*>(result.get_data()));
