362394bc01eb666af40f4a2a5ce3dd050a2bb604
MClarkTurner <mclarktu@fandm.edu>
2020-02-27 22:02:28 -0500

    c++ parser

--- itr_sklearn_batch.py
+++ itr_sklearn_batch.py
@@ -76,14 +76,14 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 	for iteration in range(repeat):
 		print("Processing depth: {:d}, iter: {:d}/{:d}".format(layer, iteration, repeat))
 	
-		num_classes = 3
+		num_classes = 1#3
 		
 		save_dir = os.path.join(dataset_dir, 'svm_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id))
 		if (not os.path.exists(save_dir)):
 			os.makedirs(save_dir)
 
 		print("process_data")
-		parse_data = False
+		parse_data = True
 		if(parse_data):
 			process_data(dataset_dir, model_type, dataset_type, dataset_id, layer, csv_filename, num_classes, num_procs)
 		
