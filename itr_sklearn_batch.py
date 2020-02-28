16330cd5c803f006810892695c4a5d27b8039e44
MClarkTurner <mclarktu@fandm.edu>
2020-02-28 00:01:30 -0500

    c++ parser

--- itr_sklearn_batch.py
+++ itr_sklearn_batch.py
@@ -184,7 +184,7 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 
 			batch_data, batch_label = train_batcher.get_batch()
 			clf.partial_fit(batch_data, batch_label, classes=np.arange(num_classes))
-			print(train_batcher.epoch, epoch)
+			print(train_batcher.epoch, cur_epoch)
 			if(train_batcher.epoch != cur_epoch):
 				print("TRAIN {0}/{1}: ".format(train_batcher.epoch, n_iter, time.time() - t_i))
 				cur_epoch = train_batcher.epoch
