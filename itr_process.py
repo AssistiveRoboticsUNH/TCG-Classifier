ff086f12b7919878af921936f60f10b562186079
MClarkTurner <mclarktu@fandm.edu>
2020-02-27 22:02:59 -0500

    c++ parser

--- itr_process.py
+++ itr_process.py
@@ -25,9 +25,10 @@ from sklearn.pipeline import Pipeline
 def extract_wrapper(ex):
 
 	out = itr_parser.extract_itr_seq_into_counts(ex['b_path'])
-	out = out.reshape(out.shape[0], -1).astype(np.uint8)
 
 	print(out.shape)
+	out = out.reshape(out.shape[0], -1).astype(np.uint8)
+	
 	#out = scipy.sparse.csr_matrix(out)
 	np.save(ex['sp_path'], out)
 
