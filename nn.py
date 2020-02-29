edb8b8f8e8794ab44355a8e9f29aa4375dd8edf4
MClarkTurner <mclarktu@fandm.edu>
2020-02-29 18:41:47 -0500

    c++ parser

--- nn.py
+++ nn.py
@@ -1,109 +1,210 @@
-import tensorflow as tf 
-
-
-
-from collections import Counter
-
-from sets import Set
 import os, sys, math, time
 import numpy as np
+from collections import Counter
 
 sys.path.append("../IAD-Generator/iad-generation/")
 from csv_utils import read_csv
 
+from sklearn import metrics
+from sklearn.linear_model import SGDClassifier
+
+import scipy
+import matplotlib
+import matplotlib.pyplot as plt
+
 from itr_sklearn import ITR_Extractor
 
-import torch
+from itr_process import process_data, retrieve_data
+
+from sklearn.feature_extraction.text import TfidfVectorizer, TfidfTransformer, HashingVectorizer, CountVectorizer
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+import random
+
+from joblib import dump, load
+import torch
 
 
-def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer, num_classes, repeat=1, parse_data=True):
+
+def save_model(clf, name):
+	dump(clf, name+'.joblib') 
+
+def load_model(name):
+	return load(name+'.joblib') 
+
+from multiprocessing import Pool
+
+class BatchParser:
+	def __init__(self, dataset, batch_size=1, shuffle=False, num_procs=1):
+		self.dataset = dataset
+		self.batch_size = batch_size
+		self.shuffle = shuffle
+		self.i = 0
+		self.epoch = 0
+		self.pool = Pool(num_procs)
+		self.pipe = None
+
+		if(self.shuffle):
+			random.shuffle(self.dataset)
+
+	def get_batch(self):
+
+		end = self.i+self.batch_size 
+		if(end > len(self.dataset)):
+			print("resample")
+			diff = len(self.dataset) - end
+
+			batch = self.dataset[self.i: len(self.dataset)] + self.dataset[0: diff]
+			self.epoch += 1
+
+
+			if(self.shuffle):
+				random.shuffle(self.dataset)
+			self.i = 0
+
+		else:
+			print("no resample")
+			t_s = time.time()
+			batch = self.dataset[self.i:end]
+			self.i = end
+			print("get files: ", time.time()-t_s)
+
+		return self.parse_batch(batch)
+
+	def get_sized_batch(self, batch_size):
+		data, label = [], []
+		for file in self.dataset[:batch_size]:
+			data.append( np.load(file['sp_path']) )
+			label.append( file['label'] )
+
+		#print("min: {0}, max: {1}".format(np.array(data).min(), np.array(data).max()))
+		print(np.array(data).shape)
+
+		return scipy.sparse.csr_matrix( np.array(data) )
+
+	def parse_batch(self, batch):
+		
+
+
+
+	def assign_pipe(self, pipeline):
+		self.pipe = pipeline
+
+class MyDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, dataset, root_dir, transform=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+		self.dataset = dataset
+		self.pipe = None
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+
+
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        t_s  =time.time()
+		data, label = [], []
+		for file in self.dataset[idx]:
+			data.append( np.load(file['sp_path']) )
+			label.append( file['label'] )
+
+
+		if(self.pipe != None):
+			data = self.pipe.transform(data)
+
+
+        sample = {'data': np.array(data), 'label': np.array(label)}
+
+        #if self.transform:
+        #    sample = self.transform(sample)
+
+        return sample
+
+
+def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer, num_classes, repeat=1, parse_data=True, num_procs=1):
 
 	max_accuracy = 0
 
 	for iteration in range(repeat):
 		print("Processing depth: {:d}, iter: {:d}/{:d}".format(layer, iteration, repeat))
 	
-		num_classes = 10
-
-
-		tcg = ITR_Extractor(num_classes, num_procs=4)		
-		
-		#open files
-		try:
-			csv_contents = read_csv(csv_filename)
-		except:
-			print("ERROR: Cannot open CSV file: "+ csv_filename)
-
-		path = 'b_path_{0}'.format(layer)
-		for ex in csv_contents:
-			ex[path] = os.path.join(dataset_dir, 'b_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id), '{0}_{1}.b'.format(ex['example_id'], layer))
-
-		train_data = [ex for ex in csv_contents if ex['dataset_id'] >= dataset_id]
-		test_data  = [ex for ex in csv_contents if ex['dataset_id'] == 0]
-
-		train_data = [ex for ex in train_data if ex['label'] < num_classes]
-		test_data = [ex for ex in test_data if ex['label'] < num_classes]
-
+		num_classes = 20
 		
 		save_dir = os.path.join(dataset_dir, 'svm_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id))
 		if (not os.path.exists(save_dir)):
 			os.makedirs(save_dir)
 
-		train_filename = os.path.join(dataset_dir, 'b_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id), 'train_{0}_{1}.npz'.format(ex['example_id'], layer))
-		test_filename  = os.path.join(dataset_dir, 'b_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id), 'test{0}_{1}.npz'.format(ex['example_id'], layer))
-
-		parse_data = not os.path.exists(train_filename)
-
-
+		parse_data = False
 		if(parse_data):
-			# TRAIN
-			scaler = StandardScaler(with_mean=False)
-			in_files = [ex[path] for ex in train_data]
-			in_labels = [ex['label'] for ex in train_data]
-
-			print("adding train data...{0}".format(len(train_data)))
-			t_s = time.time()
-			tcg.add_files_to_corpus(in_files, in_labels)
-			print("data added. time: {0}".format(time.time() - t_s))
-
-
-			data_in = tcg.tfidf.fit_transform(tcg.corpus)
-			data_in = scaler.fit_transform(data_in)
-
-			data_in = np.array(data_in.toarray())
-			data_label = np.array(tcg.labels)
-
-			np.savez_compressed(train_filename, data=data_in, label=data_label)
-
-
-			in_files = [ex[path] for ex in test_data]
-			in_labels = [ex['label'] for ex in test_data]
-			print("adding eval data...{0}".format(len(train_data)))
-			t_s = time.time()
-			tcg.add_files_to_eval_corpus(in_files, in_labels)
-			print("data added. time: {0}".format(time.time() - t_s))
-
-			eval_in = tcg.tfidf.transform(tcg.evalcorpus)
-			eval_in = scaler.fit_transform(eval_in)
-
-			eval_in = np.array(eval_in.toarray())
-			eval_label = np.array(tcg.evallabels)
-
-			np.savez_compressed(test_filename, data=eval_in, label=eval_label)
-
-		else:
-			f = np.load(train_filename)
-			data_in, data_label = f["data"], f["label"]
-			f = np.load(test_filename)
-			eval_in, eval_label = f["data"], f["label"]
-
-
+			process_data(dataset_dir, model_type, dataset_type, dataset_id, layer, csv_filename, num_classes, num_procs)
 		
-		print("fitting model...")
-		print("data_in.shape", data_in.shape)
-		print("data_label.shape", data_label.shape)
 
+		batch_size = 1000
+
+		try:
+			csv_contents = read_csv(csv_filename)
+		except:
+			print("ERROR: Cannot open CSV file: "+ csv_filename)
+
+		for ex in csv_contents:
+			ex['sp_path'] = os.path.join(dataset_dir, 'sp_{0}_{1}_{2}'.format(model_type, dataset_type, dataset_id), '{0}_{1}.npy'.format(ex['example_id'], layer))
+
+		train_data = [ex for ex in csv_contents if ex['dataset_id'] >= dataset_id]
+		train_data = [ex for ex in train_data if ex['label'] < num_classes]
+		train_batcher = MyDataset(train_data)
+
+		test_data = [ex for ex in csv_contents if ex['dataset_id'] == 0]
+		test_data = [ex for ex in test_data if ex['label'] < num_classes]
+		test_batcher = MyDataset(test_data)
+
+		trainloader = torch.utils.data.DataLoader(train_batcher, batch_size=10,
+                                          shuffle=True, num_workers=2)
+
+		testloader = torch.utils.data.DataLoader(test_batcher, batch_size=10,
+                                          shuffle=False, num_workers=2)
+
+
+		print("Training Dataset Size: {0}".format(len(train_data)))
+		print("Evaluation Dataset Size: {0}".format(len(test_data)))
+
+
+
+
+		#hashvect = CountVectorizer(token_pattern=r"\d+\w\d+")#HashingVectorizer(n_features=2**17, token_pattern=r"\d+\w\d+")
+		tfidf = TfidfTransformer(sublinear_tf=True)
+		scale = StandardScaler(with_mean=False)
+
+
+		pipe = Pipeline([
+			('tfidf', tfidf),
+			('scale', scale),
+		])
+
+		'''
+		#enabled for TRN/I3D not for TSm
+		#apply processing
+		data_standard = train_batcher.get_sized_batch(batch_size*15)
+		pipe.fit(data_standard)
+
+
+		train_batcher.assign_pipe(pipe)
+		test_batcher.assign_pipe(pipe)
+		'''
+		#from thundersvm import SVC
+		#clf = SVC(max_iter=1000, tol=1e-4, probability=True, kernel='linear', decision_function_shape='ovr')
+		
 
 		batch_size = 10
 
@@ -139,16 +240,19 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 				super(Net, self).__init__()
 				n_hidden= 1024
 
-				self.dense1 = nn.Linear(input_size, n_hidden)
-				self.dense2 = nn.Linear(n_hidden, num_classes)	
+				#self.dense1 = nn.Linear(input_size, n_hidden)
+				#self.dense2 = nn.Linear(n_hidden, num_classes)	
 
-				#self.dense = nn.Linear(input_size, num_classes)				
+				self.dense = nn.Linear(input_size, num_classes)				
 
 			def forward(self, x):
-				#return self.dense(x)
-
+				return self.dense(x)
+				'''
 				x = self.dense1(x)#.double()
 				return self.dense2(x)#.double()
+				'''
+
+		data_in = np.load(train_data[0]['sp_path'])
 
 		net = Net(data_in.shape[1], num_classes).to(device)
 
@@ -244,6 +348,8 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		#print("Training layer: {:d}, iter: {:d}/{:d}, acc:{:0.4f}, max_acc: {:0.4f}".format(layer, iteration, repeat, cur_accuracy, max_accuracy))
 		
 
+
+
 if __name__ == '__main__':
 	import argparse
 	parser = argparse.ArgumentParser(description='Generate IADs from input files')
@@ -265,21 +371,22 @@ if __name__ == '__main__':
 
 	if(FLAGS.model_type == 'i3d'):
 		from gi3d_wrapper import DEPTH_SIZE, CNN_FEATURE_COUNT
+	if(FLAGS.model_type == 'rn50'):
+		from rn50_wrapper import DEPTH_SIZE, CNN_FEATURE_COUNT
 	if(FLAGS.model_type == 'trn'):
 		from trn_wrapper import DEPTH_SIZE, CNN_FEATURE_COUNT
 	if(FLAGS.model_type == 'tsm'):
 		from tsm_wrapper import DEPTH_SIZE, CNN_FEATURE_COUNT
 
-
-	for layer in range(2):#DEPTH_SIZE):
-		main(FLAGS.model_type,
-			FLAGS.dataset_dir, 
-			FLAGS.csv_filename,
-			FLAGS.dataset_type,
-			FLAGS.dataset_id,
-			layer,
-			FLAGS.num_classes,
-			FLAGS.repeat
-			)
-	
-	
+	layer = DEPTH_SIZE-1
+	main(FLAGS.model_type,
+		FLAGS.dataset_dir, 
+		FLAGS.csv_filename,
+		FLAGS.dataset_type,
+		FLAGS.dataset_id,
+		layer,
+		FLAGS.num_classes,
+		FLAGS.repeat,
+		FLAGS.parse_data,
+		FLAGS.num_procs
+		)
