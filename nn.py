05563503c49698c8754fbc4a356774010a3c347f
MClarkTurner <mclarktu@fandm.edu>
2020-03-01 10:21:17 -0500

    c++ parser

--- nn.py
+++ nn.py
@@ -57,7 +57,7 @@ class MyDataset(Dataset):
 
 		
 		if (scaler == None):
-			self.scaler= MinMaxScaler()
+			self.scaler= StandardScaler(with_mean=False) #MinMaxScaler()
 
 			num = 1000
 			for i in range(0, len(self.dataset), num):
