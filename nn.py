2d203d52d44ab890b62fae85eab1504cabf0b4b5
MClarkTurner <mclarktu@fandm.edu>
2020-03-01 11:08:05 -0500

    c++ parser

--- nn.py
+++ nn.py
@@ -162,8 +162,8 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		train_data = [ex for ex in csv_contents if ex['dataset_id'] >= dataset_id]
 		train_data = [ex for ex in train_data if ex['label'] < num_classes]
 
-		#count_limit = 200#500
-		#train_data = [ex for ex in train_data if ex['class_count'] < count_limit]
+		count_limit = 100#500
+		train_data = [ex for ex in train_data if ex['class_count'] < count_limit]
 
 
 		print("Training Dataset Size: {0}".format(len(train_data)))
