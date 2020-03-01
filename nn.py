747d083aa416a4226f1d4216ac4ffb51118c7cf6
MClarkTurner <mclarktu@fandm.edu>
2020-03-01 04:39:55 -0500

    c++ parser

--- nn.py
+++ nn.py
@@ -264,11 +264,11 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		import torch.optim as optim
 
 		criterion = nn.CrossEntropyLoss()
-		optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+		optimizer = optim.SGD(net.parameters(), lr=0.0005, momentum=0.9)
 
 
 		t_s = time.time()
-		for epoch in range(10):  # loop over the dataset multiple times
+		for epoch in range(20):  # loop over the dataset multiple times
 
 			running_loss = 0.0
 			for i, data in enumerate(trainloader, 0):
