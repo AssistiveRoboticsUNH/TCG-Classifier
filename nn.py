513911838e2a1a8949f62133bc8378444f5b2b1c
MClarkTurner <mclarktu@fandm.edu>
2020-03-01 04:10:51 -0500

    c++ parser

--- nn.py
+++ nn.py
@@ -237,7 +237,7 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		class Net(nn.Module):
 			def __init__(self, input_size, num_classes):
 				super(Net, self).__init__()
-				n_hidden = 16
+				n_hidden = 8#16
 
 				self.dense1 = nn.Linear(input_size, n_hidden)
 				self.dense2 = nn.Linear(n_hidden, num_classes)	
