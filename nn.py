f3a6b05026e1c5b86ab4e0313a17b5b35b44bb4d
MClarkTurner <mclarktu@fandm.edu>
2020-03-01 21:10:10 -0500

    c++ parser

--- nn.py
+++ nn.py
@@ -187,7 +187,7 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 	batch_size_g = 1000
 	train_limit_g = 100000#400
 	num_classes_g = 174
-	alpha_g = 0.001
+	alpha_g = 0.0001
 	l2_norm_g = 0.25#0.5
 	l1_norm_g = 0##0.0001
 	n_hidden_g = 64#128
