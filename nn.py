d669da907f2f404da4f8876f07a3b9914b4f761e
MClarkTurner <mclarktu@fandm.edu>
2020-03-01 23:25:25 -0500

    c++ parser

--- nn.py
+++ nn.py
@@ -191,7 +191,7 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 	l2_norm_g = 0.25#0.5
 	l1_norm_g = 0##0.0001
 	n_hidden_g = 64#128
-	epoch_g = 50
+	epoch_g = 150
 
 	parse_data = False
 
