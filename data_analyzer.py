54fd6b42993e0542f9253f2530221f27ab96c560
MClarkTurner <mclarktu@fandm.edu>
2020-03-10 09:32:27 -0400

    data analyszer

--- data_analyzer.py
+++ data_analyzer.py
@@ -236,13 +236,11 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 				print("len DB: ", len(self.csv_list))
 
 			def __iter__(self):
-				print("iter")
 				self.a = 0
 				return self
 
 			#def __next__(self):
 			def next(self):
-				print("next")
 				if(self.a <= len(self.csv_list)):
 					x = self.open_file(self.csv_list[self.a])
 					self.a += 1
