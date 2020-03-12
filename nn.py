c4214dfb58ef46282a36a5416d411805f7d8b14e
MClarkTurner <mclarktu@fandm.edu>
2020-03-12 09:16:48 -0400

    data analyszer

--- nn.py
+++ nn.py
@@ -289,7 +289,7 @@ def train(net, trainloader, testloader, device, num_epochs=10, alpha=0.0001, mod
 
 		t_s = time.time()
 		for i, data in enumerate(trainloader, start=0):
-			print(i, time.time()-t_s)
+			#print(i, time.time()-t_s)
 			t_s = time.time()
 
 			# get the inputs; data is a list of [inputs, labels]
