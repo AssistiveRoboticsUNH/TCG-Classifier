d46e3079c19b76ba9d9150f656570cc12074855e
MClarkTurner <mclarktu@fandm.edu>
2020-03-16 13:33:14 -0400

    data analyszer

--- svm.py
+++ svm.py
@@ -288,7 +288,7 @@ def main(model_type, dataset_dir, csv_filename, dataset_type, dataset_id, layer,
 		num_classes,
 		parse_data, num_procs):
 
-	num_classes = 75#174#3
+	num_classes = 60#174#3
 	examples_per_class = 50#100000#100#50
 
 	train_param_list = Params(num_classes=num_classes, examples_per_class=examples_per_class)
