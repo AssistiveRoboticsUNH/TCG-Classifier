4398d7985a0bf1f27882ba8738dde58e58cb8478
MClarkTurner <mclarktu@fandm.edu>
2020-03-31 08:54:52 -0400

    preprocess itrs

--- svm2.py
+++ svm2.py
@@ -289,6 +289,7 @@ def train(net, trainloader, testloader, device, num_classes, num_epochs=10, alph
 			inp_data, inp_label = batch['data'].numpy(), batch['label'].numpy().reshape(-1)
 			
 			print("inp_data:", inp_data.shape, "inp_label:", inp_label.shape)
+			
 			inp_data = scipy.sparse.coo_matrix(np.array(inp_data))
 			inp_label = np.array(inp_label)
 
