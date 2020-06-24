18d8936f1fdbd1622c17afad8514aac794b48d08
MClarkTurner <mclarktu@fandm.edu>
2020-06-24 17:30:13 -0400

    test bottleneck

--- itr_process.py
+++ itr_process.py
@@ -45,6 +45,8 @@ def convert_event_to_itr(csv_contents, num_procs=1, empty_locs=[]):
 
 	t_s = time.time()
 	pool = Pool(num_procs)
+
+	print("convert_event_to_itr: ", len(csv_contents))
 	for i, c in enumerate(pool.imap_unordered( extract_wrapper, csv_contents, chunksize=10 )):
 		if(i % 1000 == 0):
 			print("elapsed time {0}: {1}".format(i,  time.time()-t_s))
