ffa70235d8ff3a9b28d2779f7f1bfa0f5ac3e619
MClarkTurner <mclarktu@fandm.edu>
2020-06-25 22:38:13 -0400

    test bottleneck

--- itr_parser.cpp
+++ itr_parser.cpp
@@ -84,7 +84,6 @@ bool compareEvents(Event e1, Event e2)
 
 
 vector<Event> read_sparse_matrix(string filename, int& num_features){
-	cout << "read file:" << filename << endl;
 	vector<Event> event_list;
 	
 	//open file
@@ -95,8 +94,6 @@ vector<Event> read_sparse_matrix(string filename, int& num_features){
 	if (file.is_open())
 	    file.read ((char*)&num_f, sizeof(num_f));
 	num_features = num_f;
-
-	cout << "num_features:" << num_features << endl;
 
 	if(num_features > 1000)
 		exit(-1);
@@ -157,8 +154,6 @@ np::ndarray extract_itr_seq_into_counts(string txt_file){
 	int num_features;
 	vector<Event> events = read_sparse_matrix(txt_file, num_features);
 	sort(events.begin(), events.end(), compareEvents);
-
-	cout << "num_features:" << num_features << endl;
 
 	// get a list of all of the ITRs in the txt_file
 	p::tuple shape = p::make_tuple(num_features, num_features, 7);
